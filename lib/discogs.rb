ec8bc1b53beaadfdec1ba9fb0aa3f7515d50a733
Nikky Southerland <nikky@uw.edu>
2013-01-13 10:58:04 -0800

    Add comments

--- lib/discogs.rb
+++ lib/discogs.rb
@@ -2,8 +2,8 @@ require 'json'
 require 'net/http'
 
 class Discogs
-  attr_reader :response_code
-  USER_AGENT = "Celluloid/0.1 +http://github.com/allynfolksjr/Celluloid"
+  attr_reader :response_code # The most recent HTTP response code
+  USER_AGENT = "Celluloid/0.1 +http://github.com/allynfolksjr/Celluloid" # Unique client ID
   API_URL = "http://api.discogs.com/"
   def make_get_request(request)
     url = URI.parse("#{API_URL}#{request}")
@@ -12,8 +12,8 @@ class Discogs
     res = Net::HTTP.start(url.host,url.port) { |http|
       http.request(req)
     }
-    @response_code = res.code.to_i
-    JSON.parse(res.body)
+    @response_code = res.code.to_i # It's a string otherwise. Annoying.
+    JSON.parse(res.body) # Send a nicely-parsed JSON object back
   end
   def make_image_request(request,type,filename)
     Dir.mkdir("./images") unless File::directory?("./images")
@@ -21,6 +21,7 @@ class Discogs
     url = URI.parse(request.to_s)
     Net::HTTP.start(url.host,url.port) do |http|
       response = http.get(url.path)
+      # It will write a file every time. I suppose we could check exists?
       open("./images/#{type}/#{filename}.jpg", "wb") do |file|
         file.write(response.body)
       end
@@ -28,48 +29,54 @@ class Discogs
   end
   def make_album_request(id)
     request = "/releases/#{id}"
-    result = Album.new(make_get_request(request))
-    result
+    Album.new(make_get_request(request)) # shoves json into Album instance
   end
   def make_artist_request(id)
     request = "/artists/#{id}"
-    result = Artist.new(make_get_request(request))
-    result
+    Artist.new(make_get_request(request)) # shoves json into Artist instance
   end
+  # Not fully implemented yet
   def make_artist_releases_request(id)
     request = "/artists/#{id}/releases"
     make_get_request(request)
   end
+  # This will only grab the first image and ignores the others
   def make_artist_art_request(id)
     result = make_artist_request(id)
     make_image_request(result.images.first["uri"],"artists",id)
   end
+   # This will only grab the first image and ignores the others
   def make_album_art_request(id)
      result = make_album_request(id)
      make_image_request(result.images.first["uri"],"albums",id)
   end
+  # Not implemented
   def search(terms)
   end
+  # Not implemented
   def search_results(terms)
   end
 end
 
+# Basic class that has some standard methods and attributes
 class GenericDiscogsWrapper
   attr_reader :json_hash
   def initialize(json_hash)
     @json_hash = json_hash
   end
 end
-
+# All album requests will be returned in this object
 class Album < GenericDiscogsWrapper
+  # Define a load of attributes
   %w(styles labels year images community artists id genres title master_id tracklist status release_formatted master_url released country notes companies uri formats resource_url data_quality).each do |resource|
     define_method(resource) do
       @json_hash[resource]
     end
   end
 end
-
+# All artist requests will be returned in this object
 class Artist < GenericDiscogsWrapper
+  # Define a load of attributes
   %w(id name realname profile data_quality namevariations aliases urls images).each do |resource|
     define_method(resource) do
       @json_hash[resource]

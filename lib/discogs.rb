dff0a44f0e79ec1eed13b22d6146730ee09a0362
Nikky Southerland <nikky@uw.edu>
2013-01-13 16:30:13 -0800

    Broken search code

--- lib/discogs.rb
+++ lib/discogs.rb
@@ -5,15 +5,17 @@ class Discogs
   attr_reader :response_code # The most recent HTTP response code
   USER_AGENT = "Celluloid/0.1 +http://github.com/allynfolksjr/Celluloid" # Unique client ID
   API_URL = "http://api.discogs.com/"
-  def make_get_request(request)
+  def make_get_request(request,request_params=nil)
     url = URI.parse("#{API_URL}#{request}")
+    url.query = URI.encode_www_form(request_params) unless request_params.nil?
     req = Net::HTTP::Get.new(url.path)
     req['User-Agent'] = USER_AGENT
     res = Net::HTTP.start(url.host,url.port) { |http|
-      http.request(req)
+      http.request(url)
     }
     @response_code = res.code.to_i # It's a string otherwise. Annoying.
     JSON.parse(res.body) # Send a nicely-parsed JSON object back
+   url.query
   end
   def make_image_request(request,type,filename)
     Dir.mkdir("./images") unless File::directory?("./images")
@@ -27,7 +29,7 @@ class Discogs
       end
     end
   end
-  def make_album_request(id)
+  def make_album_reques(id)
     request = "/releases/#{id}"
     Album.new(make_get_request(request)) # shoves json into Album instance
   end
@@ -46,16 +48,18 @@ class Discogs
     make_image_request(result.images.first["uri"],"artists",id)
   end
    # This will only grab the first image and ignores the others
-  def make_album_art_request(id)
+   def make_album_art_request(id)
      result = make_album_request(id)
      make_image_request(result.images.first["uri"],"albums",id)
-  end
+   end
   # Not implemented
-  def search(terms)
-  end
-  # Not implemented
-  def search_results(terms)
-  end
+  def search(params)
+    request = "/database/search"
+    make_get_request(request,params)
+end
+def artist_search(param)
+  search({"artist" => param})
+end
 end
 
 # Basic class that has some standard methods and attributes
@@ -67,7 +71,7 @@ class GenericDiscogsWrapper
 end
 # All album requests will be returned in this object
 class Album < GenericDiscogsWrapper
-  # Define a load of attributes
+  # Define a load of attributes.
   %w(styles labels year images community artists id genres title master_id tracklist status release_formatted master_url released country notes companies uri formats resource_url data_quality).each do |resource|
     define_method(resource) do
       @json_hash[resource]

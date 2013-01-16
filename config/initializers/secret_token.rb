caad17107f1e233a1396eb7f4d9cddaae08b2649
serened <serened@gmail.com>
2013-01-15 20:28:42 -0800

    adding rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Celluloid::Application.config.secret_token = 'ad0d515f426a2d1a18967ba6d4cb4585efa65926e0612c63a8abf22dccf56d91e559f0e1d6ae27c15a20440f272973735e0b9a701d10c1e768b13afc224930ba'

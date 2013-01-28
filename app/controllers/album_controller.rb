94f8888756808f88c79a8f8c8a2b0f2cf40a0fb3
serened <serened@gmail.com>
2013-01-27 17:48:24 -0800

    adding dynamic forms to gem file

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -6,15 +6,15 @@ class AlbumController < ApplicationController
 		@album = Album.new
 	end
 
-	def create 
-		@album = Album.new(params[:project]) 
-		if @album.save
-			flash[:notice] = "Album has been created."
-			redirect_to @album 
-		else
-	# nothing, yet end
-		end
-	end
+	# def create 
+	# 	@album = Album.new(params[:project]) 
+	# 	if @album.save
+	# 		flash[:notice] = "Album has been created."
+	# 		redirect_to @album 
+	# 	else
+	# # nothing, yet end
+	# 	end
+	# end
 
 	def show 
 		@album = Album.find(params[:id])

a5fd9f78cbeb4fde8825c2e33ba40976e8325985
serened <serened@gmail.com>
2013-02-12 11:54:19 -0800

    almost completed user function. stuck on a few parts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130210192626) do
+ActiveRecord::Schema.define(:version => 20130212035948) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -51,7 +51,10 @@ ActiveRecord::Schema.define(:version => 20130210192626) do
     t.string   "artist"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "albums", ["user_id"], :name => "index_albums_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false

24f9c21dd9c845d83a72c782a72b9578ba018617
Nikky Southerland <nikky@uw.edu>
2013-02-20 15:23:18 -0800

    Consolidate specs

--- spec/integration/searching_for_albums_spec.rb
+++ spec/integration/searching_for_albums_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 feature 'Searching for albums that exist locally'  do
-  scenario "Searching for albums that exist locally" #do
-  #   visit '/'
-  #   click_link 'Add Album'
-  #   # fill_in #blah lah
-  #   click_button 'Search for Album'
-  #   page.should have_content('Album Matches Found!')
-  # end
+  scenario "Searching for albums that exist locally"
+  scenario "Searching for albums that don't exist locally"
+  scenario "Searchin for albums that don't exist locally or remotely"
 end

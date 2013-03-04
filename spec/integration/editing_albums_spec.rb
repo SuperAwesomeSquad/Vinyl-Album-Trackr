73fcc3cbc3d9092830fc9076ea074f1d94d5666f
serened <serened@gmail.com>
2013-03-03 18:41:03 -0800

    working my way through the disaster town

--- spec/integration/editing_albums_spec.rb
+++ spec/integration/editing_albums_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 feature 'Editing Albums' do
 
 before do
-	Factory(:album, :title => 'Ill Communication', :artist=>'Beastie Boys')
+	Factory(:album, :title => 'Hello Nasty', :artist=>'Beastie Boys')
 	visit '/albums'
 	click_link 'View All Albums'
-	click_link 'Ill Communication'
+	click_link 'Hello Nasty'
 	click_link 'Edit Album'
 end
 
 scenario 'Updating an album' do
-	fill_in 'Title', :with => 'Ill Communication, Original Pressing'
+	fill_in 'Title', :with => 'Hello Nasty, Original Pressing'
 	click_button 'Update Album'
 	page.should have_content("Album has been updated.")
 	end

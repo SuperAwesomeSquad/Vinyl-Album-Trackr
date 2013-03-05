6b37edb0880edab5f595fcc060404f7d99c1f22c
serened <serened@gmail.com>
2013-03-05 11:51:15 -0800

    making some progress; check those user indexes

--- spec/integration/creating_albums_spec.rb
+++ spec/integration/creating_albums_spec.rb
@@ -1,67 +1,69 @@
+require 'spec_helper'
+
+feature 'Creating albums' do
+  before do
+    Factory(:album, :title => "Ill Communication", :artist => "Beastie Boys")
+    user = Factory(:user, :email => "celluloid@example.com")
+    user.confirm!
+
+    visit '/'
+    click_link "Add Album"
+    # message = "You need to sign in or sign up before continuing."
+    # page.should have_content(message)
+
+    # fill_in "Email", :with => "celluloid@example.com"
+    # fill_in "Password", :with => "password"
+    # click_button "Sign in"
+    # within("h2") { page.should have_content("Add Album") }
+  end
+
+  scenario "can create an album" do
+    # visit '/'
+    # click_link 'Add Album'
+    fill_in 'Title', :with => 'Hello Nasty'
+    fill_in 'Artist', :with => 'Beastie Boys'
+    click_button "Create Album"
+    page.should have_content("Hello Nasty")
+    click_link "Add This Album"
+    page.should have_content('Album has been created.')
+    album = Album.find_by_title('Hello Nasty')
+    page.current_url.should == album_url(album)
+    title = "Hello Nasty - Beastie Boys"
+    find("title").should have_content(title)
+    # within("#album #author") do
+    #   page.should have_content("Created by celluloid@example.com")
+    # end
+  end
+
+  scenario "can not create an album without a name" do
+    visit '/'
+    click_link 'Add Album'
+    click_button "Create Album"
+    page.should have_content("Album has not been created.")
+    page.should have_content("Title can't be blank")
+  end
+
+  # scenario "can not create an album without an artist" do
+  #   fill_in 'Title', :with => 'Hello Nasty'
+  #   click_button "Create Album"
+  #   page.should have_content("Album has not been created.")
+  #   page.should have_content("Artist can't be blank")
+  # end
+
 # require 'spec_helper'
 
-# feature 'Creating albums' do
-#   before do
-#     Factory(:album, :title => "Ill Communication", :artist => "Beastie Boys")
-#     user = Factory(:user, :email => "celluloid@example.com")
-#     user.confirm!
-
-#     visit '/'
-#     click_link "Add Album"
-#   #   message = "You need to sign in or sign up before continuing."
-#   #   page.should have_content(message)
-
-#   #   fill_in "Email", :with => "celluloid@example.com"
-#   #   fill_in "Password", :with => "password"
-#   #   click_button "Sign in"
-#   #   within("h2") { page.should have_content("Add Album") }
-#   # end
-
+# feature 'Creating Albums' do
 #   scenario "can create an album" do
 #     visit '/'
 #     click_link 'Add Album'
-#     fill_in 'Title', :with => 'Hello Nasty'
+#     fill_in 'Title', :with => 'Ill Communication'
 #     fill_in 'Artist', :with => 'Beastie Boys'
-#     click_button "Create Album"
-#     page.should have_content("Hello Nasty")
-#     click_link "Add This Album"
+#     click_button 'Create Album'
 #     page.should have_content('Album has been created.')
-#     album = Album.find_by_title('Hello Nasty')
-#     page.current_url.should == album_url(album)
-#     title = "Hello Nasty - Beastie Boys"
+#     album = Album.find_by_name("Ill Communication")
+#     page.current_url.should == project_url(project)
+#     title = "Ill Communication - Beastie Boys - Celluloid"
 #     find("title").should have_content(title)
-#     # within("#album #author") do
-#     #   page.should have_content("Created by celluloid@example.com")
-#     # end
-#   end
-
-#   scenario "can not create an album without a name" do
-#     visit '/'
-#     click_link 'Add Album'
-#     click_button "Create Album"
-#     page.should have_content("Album has not been created.")
-#     page.should have_content("Title can't be blank")
-#   end
-
-#   # scenario "can not create an album without an artist" do
-#   #   fill_in 'Title', :with => 'Hello Nasty'
-#   #   click_button "Create Album"
-#   #   page.should have_content("Album has not been created.")
-#   #   page.should have_content("Artist can't be blank")
-#   # end
-
 # end
-
-require 'spec_helper'
-
-feature 'Creating Albums' do
-  scenario "can create an album" do
-    visit '/'
-    click_link 'Add Album'
-    fill_in 'Title', :with => 'Ill Communication'
-    fill_in 'Artist', :with => 'Beastie Boys'
-    click_button 'Create Album'
-    page.should have_content('Album has been created.')
-end
 
 end

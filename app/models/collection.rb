54bfc577314fdae9c5c665a18360fea5f34f5497
serened <serened@gmail.com>
2013-03-09 12:17:54 -0800

    massaging some of the tests

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,4 @@
 class Collection < ActiveRecord::Base
   attr_accessible :title
+  belongs_to :user
 end
--- spec/support/collection_factory.rb
+++ spec/support/collection_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-	factory :user do
-		sequence(:email) { |n| "celluloid@example.com" }
+	factory :collection do
+		sequence(:email) { |n| "user#{n}@celluloid.com" }
 		password "password"
 		password_confirmation "password"
 	end

0e58484fed3765333dcd7e66e1db5cd870c2ae1c
Nikky Southerland <nikky@uw.edu>
2013-03-10 20:27:48 -0700

    Update db annotations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: collections
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  artist     :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Collection < ActiveRecord::Base
   attr_accessible :name, :title, :artist
 
--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: collections
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  artist     :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 require 'spec_helper'
 
 describe Collection do

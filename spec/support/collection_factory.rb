b5ee9e7ede2095d07617719618f60faa37aee9cc
Nikky Southerland <nikky@uw.edu>
2013-03-17 11:48:06 -0700

    remove some factories; tests run now

--- spec/support/collection_factory.rb
+++ spec/support/collection_factory.rb
@@ -1,5 +1,5 @@
-FactoryGirl.define do
-	factory :collection do
-		name "Testing"
-	end
-end
+# FactoryGirl.define do
+# 	factory :collection do
+# 		name "Testing"
+# 	end
+# end
--- spec/support/user_factory.rb
+++ spec/support/user_factory.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
-	factory :user do
-		sequence(:email) { |n| "celluloid@example.com" }
-		password "password"
-		password_confirmation "password"
-	end
-end
+# FactoryGirl.define do
+# 	factory :user do
+# 		sequence(:email) { |n| "celluloid@example.com" }
+# 		password "password"
+# 		password_confirmation "password"
+# 	end
+# end

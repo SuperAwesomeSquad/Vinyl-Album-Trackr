68e5e0ed66e7019c9967e0909e0ab5e5b9cb17e7
Nikky Southerland <nikky@uw.edu>
2013-03-17 19:37:11 -0700

    Fix signing in spec

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -5,14 +5,17 @@ feature 'Signing in' do
 		Factory(:user, :email => "celluloid@example.com")
 	end
 
-scenario 'Signing in via confirmation' do
-	open_email "celluloid@example.com", :with_subject => /Confirmation/
-	click_first_link_in_email
-	page.should have_content("Your account was successfully confirmed")
-	end
+# Re-add the following spec if/when user confirmation with email
+# is re-implimented
+
+# scenario 'Signing in via confirmation' do
+# 	open_email "celluloid@example.com", :with_subject => /Confirmation/
+# 	click_first_link_in_email
+# 	page.should have_content("Your account was successfully confirmed")
+# 	end
 
 scenario 'Signing in via form' do
-	User.find_by_email('celluloid@example.com').confirm!
+	# User.find_by_email('celluloid@example.com')
 	visit '/'
 	click_link 'Log In'
 	fill_in 'Email', :with => "celluloid@example.com"
